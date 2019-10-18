35bcd1b8446ceaa1a15a63ea5bd1fe84851048c6
tienfahsu <51950457+tienfahsu@users.noreply.github.com>
2019-10-18 10:12:57 +0800

    Add files via upload

---
+++ SEND_MAIL_INFO_TB.Data.sql
@@ -0,0 +1,14 @@
+﻿INSERT SEND_MAIL_INFO_TB ([CREATEDT], [CREATEUSER], [PROJECT], [UTILITYID], [UTILITYNAME], [SENDER], [SENDERNAME], [RECEIVER], [CC], [BCC], [SUBJECT], [BODY], [ISHTML], [FILENAME], [REMARK]) VALUES (CAST(N'2019-04-19 11:02:35.347' AS DateTime), N'alpha', N'PDA_IDRP', N'1', N'PDA', N'', N'', N'1006@COGMailGroup', N'', N'', N'Tracer reports', N'<html>
+	<head></head>
+	<body>
+		<span style="font-size:10.0pt">Dear All,<br />
+			<br />
+			<br /> Tracer reports 如附件.
+			<br />
+		</span>
+		<span style="font-size:10.0pt">________________________________________________________________________________________<br />
+			<br />  this email is sent by PDA IDRP batch process automatically  .<br />________________________________________________________________________________________<br />
+		</span>
+	</body>
+</html>
+', N'Y', N'', N'')
