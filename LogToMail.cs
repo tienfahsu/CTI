d739eb2d902088742dda5bdfbd4ee5640027b65b
tienfahsu <51950457+tienfahsu@users.noreply.github.com>
2020-02-15 11:43:58 +0800

    Delete LogToMail.cs

--- LogToMail.cs
+++
@@ -1,6 +0,0 @@
-   public MessageStatus Send_Log_Mail(List<TASK_ViewModel.TASK> data)
-        {
-            //var jsonDict = new Dictionary<string, object>();
-            MAIL_GROUPTB_MODEL model = new MAIL_GROUPTB_MODEL();
-            MAILAPI_MODEL Api_model = new MAILAPI_MODEL();
-            MessageStatus message = new MessageStatus() { Status = true, Message = "無資料" };
